266156f9165fef6473a3556e4cfe33067a1ff74a
NicolasRanniery <nicolasranniery123@gmail.com>
2025-10-17 08:16:22 -0300

    Configurações para o deploy da aplicação.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/curso_react",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
